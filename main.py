b5ba2b81588ce8c3e2240b95fe68985bfb863018
sumit-personal <sumitjain3033@gmail.com>
2026-04-08 14:41:58 +0530

    Added Pydantic Item

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class Item(BaseModel):
+    text : str = None
+    is_done : bool = False
+
+
 items = []
 @app.get('/')
 def root():
     return {"Hello" : "World"}
 
 @app.post('/items')
-def create_item(item : str):
+def create_item(item : Item):
     items.append(item)
     return items
 
+@app.get('/items')
+def get_items_list(limit : int = 10):
+    return items[:limit]
 
 @app.get('/item_list/{item_id}')
-def get_item(item_id : int):
+def get_item(item_id : int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
